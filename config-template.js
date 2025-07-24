0ac2db8a083c41714264215fd23222de7fd0adec
filippofiz <filippo.fiz@uptoten.it>
2025-07-24 18:25:41 +0200

    Add API configuration system and complete test page

---
+++ config-template.js
@@ -0,0 +1,8 @@
+// ISTRUZIONI: 
+// 1. Copia questo file e rinominalo in 'config.js'
+// 2. Inserisci la tua chiave API OpenAI
+// 3. NON committare mai il file config.js su GitHub!
+
+const API_CONFIG = {
+    OPENAI_API_KEY: 'inserisci-qui-la-tua-chiave-openai'
+};
